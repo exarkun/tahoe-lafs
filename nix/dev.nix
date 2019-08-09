91ab59b55405bd633e7bc45116f3197cce716010
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-08-09 16:32:57 -0400

    Pin a recent 19.03

--- nix/dev.nix
+++ nix/dev.nix
@@ -1,5 +1,6 @@
-{ pkgs ? import <nixpkgs> { overlays = [ (import ./overlays.nix) ]; } }:
+{ bootstrap_pkgs ? import <nixpkgs> { } }:
 let
+  pkgs = import ./nixpkgs.nix { pkgs = bootstrap_pkgs; };
   tahoe-lafs-lib = pkgs.python27Packages.callPackage ./tahoe-lafs.nix { };
 in
 (pkgs.python27.buildEnv.override {
---
+++ nix/nixpkgs.nix
@@ -0,0 +1,9 @@
+{ pkgs }:
+let
+  nixpkgs = fetchTarball
+  { url = "https://github.com/NixOS/nixpkgs-channels/archive/56d94c8c69f8cac518027d191e2f8de678b56088.tar.gz";
+    sha256 = "1c812ssgmnmh97sarmp8jcykk0g57m8rsbfjg9ql9996ig6crsmi";
+  };
+  args = { overlays = [ (import ./overlays.nix) ]; };
+in
+pkgs.callPackage nixpkgs args
