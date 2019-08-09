794a50085ff4a93e1c36586cc7dd7dd61a266efc
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-08-09 14:43:06 -0400

    Copy the whole tahoe-lafs expressions from nixpkgs, edit in place

--- nix/dev.nix
+++ nix/dev.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> { overlays = [ (import ./overlays.nix) ]; } }:
 let
-  tahoe-lafs-app = pkgs.python27Packages.callPackage ./tahoe-lafs.nix { };
-  tahoe-lafs-lib = pkgs.python27Packages.toPythonModule tahoe-lafs-app;
+  tahoe-lafs-lib = pkgs.python27Packages.callPackage ./tahoe-lafs.nix { };
 in
 (pkgs.python27.buildEnv.override {
   extraLibs = [ tahoe-lafs-lib ];
