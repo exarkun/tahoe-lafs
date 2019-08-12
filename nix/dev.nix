478b0c51bec27a97352732deee33ff556d5bd2ac
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-08-12 11:23:52 -0400

    This yields marginally better results

--- nix/dev.nix
+++ nix/dev.nix
@@ -2,8 +2,22 @@
 let
   pkgs = import ./nixpkgs.nix { pkgs = bootstrap_pkgs; };
   tahoe-lafs-lib = pkgs.python27Packages.callPackage ./tahoe-lafs.nix { };
+  custom-python = pkgs.python27.withPackages (ps:
+    with pkgs.python27.pkgs;
+    [ tahoe-lafs-lib
+
+      # checkInputs - non-recoverable from the tahoe-lafs derivation.
+      hypothesis
+      testtools
+      fixtures
+      treq
+
+      # Accidentally not declared by Twisted package?
+      appdirs
+
+      # Why is this unavailable???
+      pyhamcrest
+    ]
+  );
 in
-(pkgs.python27.buildEnv.override {
-  extraLibs = [ tahoe-lafs-lib ];
-  ignoreCollisions = true;
-}).env
+(custom-python.override (old: { ignoreCollisions = true; })).env
